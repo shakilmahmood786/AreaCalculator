c1603c63f3dce0dcd34a62f83bf297175e436cc5
shakilmahmood786 <40207618+shakilmahmood786@users.noreply.github.com>
2021-01-13 18:42:58 +0000

    Add files via upload

---
+++ FloatAndDouble/src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double poundToKilo = 0.45359237d;
+        double noOfPounds = 5d;
+        double convertedToKilos = (poundToKilo * noOfPounds);
+
+        System.out.println(convertedToKilos + " kg");
+
+
+
+
+//       // width of int = 32 (4 bytes)
+//       int myIntValue = 5 / 3;
+//
+//        // width of int = 32 (4 bytes)
+//       float myFloatValue = 5f / 3f;
+//
+//        // width of int = 64 (8 bytes)
+//       double myDoubleValue = 5d / 3d;
+//
+//        System.out.println("myIntValue = " + myIntValue);
+//        System.out.println("myFloatValue = " + myFloatValue);
+//        System.out.println("myDoubleValue = " + myDoubleValue);
+    }
+}
