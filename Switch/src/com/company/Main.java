e0c75984ca7b5ce4f6eb1f18241f3e81e19f1758
shakilmahmood786 <40207618+shakilmahmood786@users.noreply.github.com>
2021-01-13 19:12:20 +0000

    Add files via upload

---
+++ Switch/src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        char switchLetter = 'a';
+
+        switch (switchLetter) {
+
+            case 'a':
+                System.out.println("letter is a");
+                break;
+
+            case 'b':
+                System.out.println("letter is b");
+                break;
+
+            case 'c':
+                System.out.println("letter is c");
+                break;
+
+            case 'd':
+                System.out.println("letter is d");
+                break;
+
+            case 'e':
+                System.out.println("letter is e");
+                break;
+
+            default:
+                System.out.println("not a,b,c,d,e");
+                break;
+        }
+    }
+}
+
+
