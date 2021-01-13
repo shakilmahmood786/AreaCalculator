6803a3838625618bf339d1eabe2e9f65dd6fac6f
shakilmahmood786 <40207618+shakilmahmood786@users.noreply.github.com>
2021-01-13 19:20:45 +0000

    Add files via upload

---
+++ ThreeAndFive/src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int count = 0;
+        for (int i = 1; i <= 1000; i++) {
+
+
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+
+                sum += i;
+                System.out.println(i);
+                count++;
+
+            }
+
+            if (count == 5) {
+            break;
+
+        }
+    }
+
+        System.out.println(sum);
+
+}
+
+}
