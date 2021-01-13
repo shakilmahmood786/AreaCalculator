78199218defeede02cdd7bfca4bc2baf9bf518ca
shakilmahmood786 <40207618+shakilmahmood786@users.noreply.github.com>
2021-01-13 18:40:22 +0000

    Add files via upload

---
+++ CharAndBoolean/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // width of 16 (2 bytes)
+        char myChar = 'S';
+        char myUnicode = '\u00AE';
+        System.out.println(myUnicode);
+
+        boolean myBoolean = false;
+
+
+    }
+}
