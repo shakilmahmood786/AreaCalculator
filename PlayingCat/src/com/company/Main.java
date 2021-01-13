c09ffa123f9e823b7417b0d4d9362c4a4a117601
shakilmahmood786 <40207618+shakilmahmood786@users.noreply.github.com>
2021-01-13 19:02:49 +0000

    Add files via upload

---
+++ PlayingCat/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(isCatPlaying(true,10));
+        System.out.println(isCatPlaying(false,36));
+        System.out.println(isCatPlaying(false,35));
+
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature){
+
+       // if((temperature >= 25) && (temperature <= 35)){
+            if ((summer = true) && ((temperature >= 25) && (temperature <= 35))){
+            return true;
+
+
+        }
+
+        else if ((summer = true) && ((temperature >= 25) && (temperature <= 45)))
+        {
+
+            return true;
+        }
+
+        else
+
+            return false;
+
+    }
+
+}
