7d9b516a4b20775495ee554fee8a265a40eb6583
shakilmahmood786 <40207618+shakilmahmood786@users.noreply.github.com>
2021-01-13 18:43:46 +0000

    Add files via upload

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Shakil!");
+
+        int myFirstNumber = 5;
+        int mySecondNumber = 5;
+        int myThirdNumber = 5;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myFirstNumber);
+        System.out.println(myTotal);
+
+    }
+}
